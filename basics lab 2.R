e5f9ddb274753bea0a9de96e2f4b11adbf193462
vivianurama <vurama@wvstateu.edu>
2025-01-23 19:11:38 -0500

    Add files via upload

---
+++ basics lab 2.R
@@ -0,0 +1,70 @@
+x <- 1
+x-1
+x<-1
+X <- -1
+t <- 5
+r <- 11
+X == x
+X == X
+X!= x
+v <- 27
+class(v)
+B <- 3L
+class(B)
+Preeti <- "one"
+class(preeti)
+class(Preeti)
+x <- True ;y <- False
+x <- True ; y <- false
+x <- TRUE ; y <- FALSE
+class(TRUE)
+X <- 1+1i
+class(1+1i)
+vector <- c(1,2,34,5)
+Chikwado <- c(2:9)
+ify <- matrix (vector, nrow = 5, ncol = 2)
+vvector <- matrix(vvector, nrow = 3, ncol = 3)
+df <- data.frame(id = c("A","B","C","D","E"), values = vvector)
+num_vec <- numeric(5)
+num_vec <- c(1,2,3,4)
+num_vec[5] <- 5
+num_vec[6] <- 8
+num_vec[3]
+num_vec[3] <- num_vec[5]
+charactervec <- c("nna","chikwado","dimma","ify","ari","odi","chiboy","ezi")
+names(num_vec) <- c("first","second","third","fourth","fifth")
+num_vec
+names(charactervec) <- c("firstborn","secondborn","thirdborn","fourthborn","fifthborn")
+charactervec
+num_vec[c("second","fourth")]
+numericvector <- c(3,5,6,7,9)
+charactervector <- c("C","E","G","I","K")
+names(numericvector) <- names(charactervector)
+numericvector[c("C","E","G")]
+numericvector[c("C", "E","G")]
+new_vec <- numericvector[c(3,5,7)]
+new_vec2 <- names(new_vec)
+(x+y)*(x-y)
+y/x
+x^2
+2^(x*y)
+sqrt(x)
+log10(100)
+log(1000)
+log(x = x,base =3)#log base 3
+sqrt(16)
+Leaf_area <- c(2.5, 1.6, 2.2, 2.6)
+Leaf_mass <- c(22, 15, 20, 24)
+mean(Leaf_area)
+mean(Leaf_mass)
+median(Leaf_area)
+median(Leaf_mass)
+max(Leaf_area)
+max(Leaf_mass)
+max(Leaf_area) > max(Leaf_mass)
+min(Leaf_mass)< max(Leaf_area)
+mean(Leaf_area)> median(Leaf_mass)
+Leaf_mass/Leaf_area
+LMA <- c(Leaf_mass/Leaf_area)
+mean(Leaf_mass/Leaf_area)
+
