aefe9f0481442493c9a3b1f7b3801b7056f4dd18
vivianurama <vurama@wvstateu.edu>
2025-01-23 19:14:41 -0500

    Add files via upload

---
+++ Vivian_012125.R
@@ -0,0 +1,29 @@
+data = c(1,2,3)
+data(1,2,3)
+data(1,2,2,3)
+data=(1,2,3)
+data = c(1,2,3_)
+data(...list = (1,2,3))
+data = c(1,2,3)
+mydataframe - data.frame(
+  name = c("viv", 'urama').
+  age = c(10, 20)
+height = c(5, 5.8)  
+view (data)
+plot(age~height)
+getwd()
+setwd(/vivian/desktop)
+setwd(desktop
+      )
+install.packages(ggplot2)
+install.packages("ggplot2")
+10+20
+10+20
+1+2=
+data =c(2,4,6,8,10)
+install.packages('ggplot2')
+install.packages("ggplot2")
+installed.packages(ggplot2)
+installed.packages("ggplot2")
+plot (age~height)
+plot (height~age)
